96f778d0da0ad7c654ceb7ad972dfc74c61b3d74
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-11 20:15:14 +0900

    Added colors to css

--- react-info-site/src/index.jsx
+++ react-info-site/src/index.jsx
@@ -6,18 +6,25 @@ import '../assets/stylesheets/application.scss';
 const Home = () => {
   return (
     <div>
-      <div className="nav-bar">
-        <i className="devicon-react-original colored">React Facts</i>
-        <span>React course - project1</span>
+      <header>
+        <div className="nav-bar">
+          <i className="devicon-react-original colored">React Facts</i>
+          <span>React course - project1</span>
+        </div>
+      </header>
+      <div className="body-info">
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
       </div>
-      <h1>Fun facts about React</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jordan Walke</li>
-        <li>Has well over 100k stars on GitHub</li>
-        <li>Is maintained by Facebook</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <footer>
+        <small>"© 2021 Moahtdeep development. All rights reserved."</small>
+      </footer>
     </div>
   );
 };
