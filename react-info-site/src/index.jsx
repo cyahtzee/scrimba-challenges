83371f7e18d5a049770ace1c8f876e7cb22699fa
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-11 18:06:49 +0900

    Added Home component

--- react-info-site/src/index.jsx
+++ react-info-site/src/index.jsx
@@ -3,16 +3,26 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
+const Home = () => {
   return (
     <div>
-      Hello,
-      {name}
+      <div className="footer">
+        <i class="devicon-react-original colored">React Facts</i>
+        <span>React course - project1</span>
+      </div>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
     </div>
   );
 };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Home />, root);
 }
