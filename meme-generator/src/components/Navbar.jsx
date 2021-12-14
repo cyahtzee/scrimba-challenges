9c2196aea6433899d6ca568638d8b8a2a46729b9
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-14 23:46:59 +0900

    Added Navbar component

---
+++ meme-generator/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header className='header'>
+      <img className='header--image' src="../assets/images/troll-face.png" alt="troll-face" />
+      <h2 className='header--title'>Meme Generator</h2>
+      <h4 className='header--project'>React Course-Project3</h4>
+    </header>
+  );
+};
+
+export default Navbar;
--- meme-generator/src/index.jsx
+++ meme-generator/src/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App';
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
+const Hello = () => {
   return (
     <div>
-      Hello,
-      {name}
+      <App />
     </div>
   );
 };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<App />, root);
 }
