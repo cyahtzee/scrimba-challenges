30b4fcf7c10d77111443c1e132564dad572e145e
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-17 15:33:22 +0900

    Updated to react 17.2

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import memesData from '../memesData.js';
 
-const Form = () => {
+const Meme = () => {
+  const [memeImage, setMemeImage] = React.useState('')
+  const generateMemeImage = () => {
+    const random = Math.floor(Math.random() * memesData.data.memes.length)
+    return memesData.data.memes[random].url
+  }
+  const handleClick = () => {
+    setMemeImage(generateMemeImage)
+  }
   return (
     <main>
-      <form className="form" action="">
+      <form className="form">
         <input className="form--input" type="text" placeholder="top text" />
         <input className="form--input" type="text" placeholder="bottom text" />
-        <button className="form--button" type="submit">Get a new meme image 🖼</button>
+        <button onClick={handleClick} className="form--button" type="submit">Get a new meme image 🖼</button>
       </form>
+      <div className="meme">
+        <img src={memeImage} alt="meme" />
+      </div>
     </main>
 
   );
 };
 
-export default Form;
+export default Meme;
