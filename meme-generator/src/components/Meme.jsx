3391b73b06adecc0519ba2fa28f04c7655148c90
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-12-28 22:13:25 +0900

    Added generic form controll of the input

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -2,23 +2,61 @@ import React from 'react';
 import memesData from '../memesData.js';
 
 const Meme = () => {
-  const [memeImage, setMemeImage] = React.useState('')
+  const [meme, setMeme] = React.useState()
   const generateMemeImage = () => {
     const random = Math.floor(Math.random() * memesData.data.memes.length)
     return memesData.data.memes[random].url
   }
+
+  const [formData, setFormData] = React.useState(
+    {
+      top: "",
+      bottom: ""
+    }
+  )
+
+  console.log(formData)
+  function handleChange(event) {
+    setFormData(prevState => (
+      {
+        ...prevState,
+        [event.target.name]: event.target.value
+      }
+    ))
+  }
+
   const handleClick = () => {
-    setMemeImage(generateMemeImage)
+    setMeme(generateMemeImage)
   }
   return (
     <main>
       <div className="form">
-        <input className="form--input" type="text" placeholder="top text" />
-        <input className="form--input" type="text" placeholder="bottom text" />
-        <button onClick={handleClick} className="form--button" type="submit">Get a new meme image 🖼</button>
+        <input
+          className="form--input"
+          type="text"
+          placeholder="top text"
+          onChange={handleChange}
+          name="top"
+          value={formData.top}
+        />
+        <input
+          className="form--input"
+          type="text"
+          placeholder="bottom text"
+          onChange={handleChange}
+          name="bottom"
+          value={formData.bottom}
+        />
+        <button
+          onClick={handleClick}
+          className="form--button"
+          type="submit">Get a new meme image 🖼
+        </button>
       </div>
       <div className="meme">
-        <img src={memeImage} alt="meme" />
+        <img className="meme--image" src={meme} alt="meme" />
+        <h2 className="meme--text top">{formData.top}</h2>
+        <h2 className="meme--text bottom">{formData.bottom}</h2>
       </div>
     </main>
 
